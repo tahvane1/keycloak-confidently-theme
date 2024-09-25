b0562dacd0423112b33f5a372a281389d0731975
Erkka Tahvanainen <erkka.tahvanainen@confidently.fi>
2024-09-25 16:51:29 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
     plugins: [
         react(),
         keycloakify({
-            accountThemeImplementation: "none"
+            themeName: "confidently-meo",
+            accountThemeImplementation: "Single-Page"
         })
     ]
 });
